1476407a8c20ee3340d9134430a951315aff32ef
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-04-19 23:18:23 +0530

    Started with js Foundation

---
+++ week1/main.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+
+// Static and Dynamic Languages
+// Static :- Its has strict type
+//Dynamic :-  its is has no strict type(Superset has come that call Typescript)
+
+// javascript has single threaded nature(runs only one core )
+// There are way can multithreads
+
+//1. write program to given name for greeting
+
+// const greeting = (fname, lname) => {
+//   greet = `Welcome ${fname} ${lname} to Cohort 2.0`;
+//   return greet;
+// };
+
+// const res = greeting("Moiz", "shaikh");
+
+// console.log(res);
+
+// //2.
+
+// const greetGender = (gender) => {
+//   const greet = gender === "male" ? "He is Awesome" : "She is beautifull";
+//   return greet;
+// };
+
+// const resGender = greetGender("female");
+// console.log(resGender);
+
+// //3
+
+// for (let index = 0; index <= 1000; index++) {
+//   console.log(index);
+// }
