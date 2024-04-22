52033015fcec6bcb8b24490786e4eed24dc1003b
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-04-22 22:54:40 +0530

    Callback inProgress

--- week1/main.js
+++ week1/main.js
@@ -33,3 +33,103 @@ console.log("Hello World");
 // for (let index = 0; index <= 1000; index++) {
 //   console.log(index);
 // }
+
+// let Fname = "moiz";
+// let age = 18;
+// let isMarried = false;
+
+// console.log("Ths is person name is " + Fname + "his age is " + age);
+
+// let personArray = ["Moiz ", "gaurish", "Girish"];
+
+// personArray.forEach((ele) => {
+//   console.log(ele);
+// });
+
+const ages = [21, 22, 23, 24, 25, 26, 85, 75];
+
+// for (let i = 0; i <= ages.length; i++) {
+//   if (ages[i] % 2 === 0) {
+//     console.log(ages[i]);
+//   }
+// }
+
+// let largest = 0;
+
+// for (let i = 0; i <= ages.length; i++) {
+//   if (ages[i] > ages[i]) {
+//     console.log(ages[i]);
+//   }
+// }
+
+// let person = [
+//   { fName: "Moiz", gender: "male" },
+//   {
+//     fName: "Girish",
+//     gender: "female",
+//   },
+//   {
+//     fName: "Girish",
+//     gender: "female",
+//   },
+// ];
+
+// for (let i = 0; i < person.length; i++) {
+//   if (person[i]["gender"] === "female") {
+//     console.log(person[i]["fName"]);
+//   }
+// }
+
+// let largest = 0;
+
+// for (let i = 0; i < ages.length; i++) {
+//   if (ages[i] > largest) {
+//     largest = ages[i];
+//   }
+// }
+// console.log(largest);
+
+// let reverseArray = [];
+
+// for (let i = ages.length - 1; i >= 0; i--) {
+//   console.log(i);
+//   console.log(i < 0);
+//   console.log(i < 0);
+//   reverseArray.push(ages[i]);
+// }
+// console.log(reverseArray);
+
+// function sum(a, b, clBack) {
+//   let sum = a + b;
+//   return clBack(sum);
+// }
+// function displayResult(data) {
+//   return "This is sum of " + data;
+// }
+
+// function displayResultPassive(data) {
+//   return "Sum of data " + data;
+// }
+
+// const a = sum(2, 3, displayResult);
+// const b = sum(2, 3, displayResultPassive);
+
+// console.log(a);
+// console.log(b);
+
+// function calculate(fnType) {
+//   return fnType;
+// }
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// function minus(a, b) {
+//   return a - b;
+// }
+
+// let add = calculate(sum(2, 5));
+// let sub = calculate(minus(10, 6));
+
+// setTimeout(greetGender("male"), 5000);
