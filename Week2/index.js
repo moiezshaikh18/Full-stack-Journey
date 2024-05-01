58ff5b644d8a6321ce6e52d40ce54511b7829684
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-05-01 12:32:44 +0530

    Week 3 Started 01/05

--- Week2/index.js
+++ Week2/index.js
@@ -1,23 +1,118 @@
-const express = require("express");
-const bodyparser = require("body-parser");
+// const express = require("express");
 
-const app = express();
+// const app = express();
 
-//Middle ware
-app.use(bodyparser.json());
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// var users = [
+//   {
+//     name: "john",
+//     kidney: [
+//       {
+//         healthy: false,
+//       },
+//       {
+//         healthy: true,
+//       },
+//     ],
+//   },
+// ];
 
-app.post("/add", (req, res) => {
-  const { number } = req.body;
+// app.get("/", (req, res) => {
+//   let numberOfKidneys = users[0].kidney.length;
+//   let numberOfHealthyKidney = users[0].kidney.filter((kidney) => {
+//     if (kidney.healthy) {
+//       return kidney;
+//     }
+//   }).length;
 
-  let sum = number + number;
+//   let numberOfUnhealthyKidney = numberOfKidneys - numberOfHealthyKidney;
 
-  res.send(`${sum}`);
-});
+//   res.json({
+//     numberOfKidneys,
+//     numberOfHealthyKidney,
+//     numberOfUnhealthyKidney,
+//   });
+// });
 
-app.listen(3001, () => {
-  console.log("Running Port:" + 3001);
-});
+// app.post("/", (req, res) => {
+//   let newKidney = req.body.isHealthy;
+
+//   users[0].kidney.push({
+//     healthy: newKidney,
+//   });
+
+//   res.send("Received");
+// });
+
+// app.put("/", (req, res) => {
+//   let updateKidney = req.body.updatedKidney;
+
+//   users[0].kidney.forEach((element) => {
+//     element.healthy = updateKidney;
+//   });
+
+//   res.send("Updated");
+// });
+
+// app.delete("/", (req, res) => {
+//   // let deleteUnHealthyKideny = req.body.deleteUnHealthyKideny;
+
+//   let isUnhealthy = users[0].kidney.findIndex((_item) => _item === false);
+//   console.log(isUnhealthy);
+
+//   let deleted = users[0].kidney.filter((_item) => {
+//     return _item.healthy !== false;
+//   });
+
+//   users[0].kidney = deleted;
+//   console.log(deleted);
+
+//   res.send("Deleted");
+// });
+
+// // const calculateSumOfNumbers = (n) => {
+// //   let ans = 0;
+
+// //   for (let i = 0; i <= n; i++) {
+// //     ans = ans + i;
+// //   }
+// //   return ans;
+// // };
+
+// // app.get("/", (req, res) => {
+// //   let n = req.query.n;
+// //   let sum = calculateSumOfNumbers(n);
+// //   res.send(sum.toString());
+// // });
+
+// app.listen(3001, () => {
+//   console.log("This is New Port" + 3001);
+// });
+
+// week 2.6
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+
+// let newArray = array.filter((_item) => {
+//   return _item % 2 == 0;
+// });
+
+// // let newForArray = [];
+
+// for (let i = 0; i < array.length; i++) {
+//   let newArray = [];
+//   if (!array[i] % 2 == 0) {
+//     newArray = array.slice(i, i);
+//   }
+//   console.log("newArray", newArray);
+//   return newArray;
+// }
+
+// let myPromise = new Promise((resolve, reject) => {
+//   resolve("DONE");
+// });
+
+// myPromise.then((res) => res).catch((err) => err);
+
+// console.log("Mypromise", myPromise);
